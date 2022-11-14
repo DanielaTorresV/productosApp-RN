09f90c71fa127669287be626cb9c56955761b04b
Daniela Torres <dannytorres0211@gmail.com>
2022-11-14 18:34:37 -0500

    feat: create an app with RN and Ts

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      
+    </NavigationContainer>
+  )
+}
+
+export default App;
+
