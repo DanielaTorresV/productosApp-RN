78057276986d739ad45d9c8ba9b0b00183cb7af7
Daniela Torres <dannytorres0211@gmail.com>
2022-11-15 09:58:54 -0500

    feat: create Auth Context

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,29 @@
+import React, { createContext } from 'react';
+
+import { Usuario } from '../interfaces/AppInterfaces';
+
+
+type AuthContextProps = {
+  errorMessage: string,
+  token: string | null,
+  user: Usuario | null,
+  status: 'checking' | 'authenticated' | 'not-authenticated';
+  signUp: () => void,
+  signIn: () => void,
+  logout: () => void,
+  removeEror: () => void,
+}
+
+export const AuthContext = createContext( {} as AuthContextProps );
+
+export const AuthProvider = ({ children }: any) => {
+  return(
+    <AuthContext.Provider
+      value={{
+        
+      }}
+    >
+      { children }
+    </AuthContext.Provider>
+  )
+}
