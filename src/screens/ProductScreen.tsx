3f1a50a9a7b8a2fb32d2e59b2ddc456065e46e8b
Daniela Torres <dannytorres0211@gmail.com>
2022-11-16 09:41:03 -0500

    feat: navigate to Product Screen

--- src/screens/ProductScreen.tsx
+++ src/screens/ProductScreen.tsx
@@ -1,12 +1,37 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect } from 'react';
+import { StackScreenProps } from '@react-navigation/stack';
+import { View, Text, StyleSheet } from 'react-native';
 
-const ProductScreen = () => {
+import { ProductsStackParams } from '../navigation/ProductsNavigator';
+
+interface Props extends StackScreenProps<ProductsStackParams, 'ProductScreen'>{};
+
+const ProductScreen = ({ navigation, route }: Props) => {
+
+  const { id, name = '' } = route.params;
+
+  useEffect(() => {
+    navigation.setOptions({
+      title: (name) ? name : 'Nuevo Producto'
+    })
+  }, [])
+
   return (
-    <View>
-      <Text>Prodcut Screen</Text>
+    <View style={ styles.containerProduct } >
+      <Text style={ styles.text }>{ id }</Text>
+      <Text style={ styles.text }>{ name }</Text>
     </View>
   )
 }
 
 export default ProductScreen;
+
+const styles = StyleSheet.create({
+  containerProduct: {
+    flex: 1,
+    paddingHorizontal: 10
+  },
+  text: {
+    fontSize: 20
+  }
+});
--- src/screens/ProductsScreen.tsx
+++ src/screens/ProductsScreen.tsx
@@ -1,14 +1,32 @@
-import React, { useContext} from 'react';
+import React, { useContext, useEffect} from 'react';
 import { View, Text, FlatList, Button, StyleSheet, TouchableOpacity } from 'react-native';
+import { StackScreenProps } from '@react-navigation/stack';
 
 import { ProductsContext } from '../context/ProductsContext';
 import { AuthContext } from '../context/AuthContext';
+import { ProductsStackParams } from '../navigation/ProductsNavigator';
 
-const ProductsScreen = () => {
+
+interface Props extends StackScreenProps<ProductsStackParams, 'ProductsScreen'>{};
+
+const ProductsScreen = ({ navigation }: Props) => {
 
   const { logout } = useContext( AuthContext );
   const { products } = useContext( ProductsContext );
 
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity
+          activeOpacity={ 0.8 }
+          style={ styles.addButton }
+          onPress={ () => navigation.navigate('ProductScreen', {}) }
+        >
+          <Text style={{ color: 'white' }}>Agregar</Text>
+        </TouchableOpacity>
+      )
+    })
+  }, [])
 
   return (
     <View style={ styles.containerProducts }>
@@ -18,6 +36,10 @@ const ProductsScreen = () => {
         renderItem={({ item }) => (
           <TouchableOpacity
             activeOpacity={ 0.8 }
+            onPress={ () => navigation.navigate('ProductScreen', {
+              id: item._id,
+              name: item.nombre
+            }) }
           >
             <Text style={ styles.productName } >{ item.nombre }</Text>
           </TouchableOpacity>          
@@ -55,5 +77,12 @@ const styles = StyleSheet.create({
     borderBottomWidth: 3,
     marginVertical: 5,
     borderBottomColor: 'rgba(0,0,0,0.2)'
+  },
+  addButton: {
+    marginRight: 10,
+    backgroundColor: '#5856D6',
+    paddingVertical: 5,
+    paddingHorizontal: 5,
+    borderRadius: 5 
   }
 });
