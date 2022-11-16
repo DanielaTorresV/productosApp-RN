f4200154491023dbbbb427876ed63564d2ed26f8
Daniela Torres <dannytorres0211@gmail.com>
2022-11-16 10:01:39 -0500

    feat: design product screen

--- src/screens/ProductScreen.tsx
+++ src/screens/ProductScreen.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect } from 'react';
 import { StackScreenProps } from '@react-navigation/stack';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
 
 import { ProductsStackParams } from '../navigation/ProductsNavigator';
+import { ScrollView } from 'react-native-gesture-handler';
 
 interface Props extends StackScreenProps<ProductsStackParams, 'ProductScreen'>{};
 
@@ -18,8 +19,42 @@ const ProductScreen = ({ navigation, route }: Props) => {
 
   return (
     <View style={ styles.containerProduct } >
-      <Text style={ styles.text }>{ id }</Text>
-      <Text style={ styles.text }>{ name }</Text>
+      <ScrollView>
+        <Text style={ styles.label } >Nombre del Producto:</Text>
+        <TextInput 
+          placeholder='Producto'
+          //Todo: Value, onChange
+          style={ styles.textInput }
+        />
+
+        {/* Picker / Selector */}
+        <Text style={ styles.label } >Categoría:</Text>
+
+
+        <Button 
+          title='Guardar'
+          color='#5856D6'
+          //TODO: Pro hacer
+          onPress={ () => {} }
+        />
+
+        <View style={ styles.containerButtons }>
+        <Button 
+          title='Cámara'
+          color='#5856D6'
+          //TODO: Pro hacer
+          onPress={ () => {} }
+        />
+        <View style={{ width: 40 }} />
+        <Button 
+          title='Galería'
+          color='#5856D6'
+          //TODO: Pro hacer
+          onPress={ () => {} }
+        />
+        </View>
+
+      </ScrollView>
     </View>
   )
 }
@@ -29,9 +64,26 @@ export default ProductScreen;
 const styles = StyleSheet.create({
   containerProduct: {
     flex: 1,
-    paddingHorizontal: 10
+    marginTop: 10,
+    paddingHorizontal: 20
   },
-  text: {
-    fontSize: 20
+  label: {
+    fontSize: 20,
+  },
+  textInput: {
+    fontSize: 17,
+    marginTop: 5,
+    marginBottom: 15,
+    height: 45,
+    borderWidth: 1,
+    borderColor: 'rgba(0,0,0,0.2)',
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    borderRadius: 20,    
+  },
+  containerButtons: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginTop: 20
   }
 });
